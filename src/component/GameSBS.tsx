7fd7909613851c0c576b0a13cf06363980b5be62
powen <powen0966091716@gmail.com>
2024-11-06 15:40:25 +0800

    SBS game in developing

--- src/component/GameSBS.tsx
+++ src/component/GameSBS.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { SBS_State } from "../enum/GameSBS_Enum"
+
 export default function GameSBS(){
     return (
         <div className="Container Cyan-Shadow">
@@ -9,9 +12,30 @@ export default function GameSBS(){
     )
 }
 
-function GameSBS_Content(){
+function GameSBS_Content() {
+    const [sbs_state, set_sbs_state] = useState<SBS_State>(SBS_State.ready);
+
+    function getImage(): string {
+        const baseImgPath = "src/assets/SBS/";
+        switch (sbs_state) {
+            case SBS_State.ready:
+                return `${baseImgPath}10s.png`;
+            case SBS_State.operating:
+                return `${baseImgPath}operating.png`;
+            case SBS_State.failed:
+                return `${baseImgPath}failed.jpg`;
+            case SBS_State.switching:
+                return `${baseImgPath}switching.png`;
+            case SBS_State.succeed:
+                return `${baseImgPath}SBS.gif`;
+            default:
+                return "";
+        }
+    }
+
     return (
         <div>
+            <img src={getImage()} alt="SBS State" />
         </div>
-    )
+    );
 }
--- src/enum/GameSBS_Enum.tsx
+++ src/enum/GameSBS_Enum.tsx
@@ -1,4 +1,4 @@
-enum SBS_State {
+export enum SBS_State {
     ready = "連續快速點擊 [s] 幫桐人撐十秒",
     operating = "操作中...",
     failed = "哥們，連十秒都不行😅",
