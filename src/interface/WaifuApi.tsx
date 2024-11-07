424d24dac01057fa0ca7b656c2a8bb6e28fa522e
powen <powen0966091716@gmail.com>
2024-11-07 09:57:38 +0800

    merge from Dev (#1)
    * Update App.css
    
    * rename NavBar to Header
    
    * fix typo
    
    * update member data and add grouppic
    
    * add SBS assets
    
    * Update operating.png
    
    * rename Web title to  Team name
    
    * SBS game in developing
    
    * SBS game in testing
    
    * update GameSBS_Content
    
    * update SBS img css
    
    * Update App.css
    
    * update App.css
    
    * update SBS
    
    * GameSBS
    
    * Update GameSBS.tsx
    
    * now use fading instead of flashing
    
    * better look SBS
    
    * Update App.css
    
    * Update App.css
    
    * SBS time sec update
    
    * Update GameSBS.tsx
    
    * ApiTest in developing

---
+++ src/interface/WaifuApi.tsx
@@ -0,0 +1,38 @@
+export interface Tag {
+    description: string;
+    is_nsfw: boolean;
+    name: string;
+    tag_id: number;
+}
+
+export interface Artist {
+    artist_id: number;
+    deviant_art: string;
+    name: string;
+    patreon: string;
+    pixiv: string;
+    twitter: string;
+}
+
+export interface Image {
+    artist: Artist;
+    byte_size: number;
+    dominant_color: string;
+    extension: string;
+    favorites: number;
+    height: number;
+    image_id: number;
+    is_nsfw: boolean;
+    liked_at: string;
+    preview_url: string;
+    signature: string;
+    source: string;
+    tags: Tag[];
+    uploaded_at: string;
+    url: string;
+    width: number;
+}
+
+export interface ImageData {
+    images: Image[];
+}
